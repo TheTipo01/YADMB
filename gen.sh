a3830f0f8e83f894f537bbbb669f58dd0219e31b
Manuel <10187614+TheTipo01@users.noreply.github.com>
2020-08-04 02:37:56 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-youtube-dl -o ./download/$2.m4a -f 140 $1
-ffmpeg -i ./download/$2.m4a -f s16le -ar 48000 -ac 2 pipe:1 | dca > ./audio_cache/$2.dca
+ffmpeg -i download/$1.m4a -f s16le -ar 48000 -ac 2 pipe:1 | dca > audio_cache/$1.dca
