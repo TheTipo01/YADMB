429913eb9bdd6c64317d0927abca2b8da0c9437f
Manuel Raimo <thetipo01@protonmail.com>
2023-06-09 10:23:30 +0200

    feat: search youtube client

--- youtube/youtube.go
+++ youtube/youtube.go
@@ -2,6 +2,7 @@ package youtube
 
 import (
 	"context"
+	"github.com/bwmarrin/lit"
 	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
 	"strings"
@@ -25,7 +26,12 @@ func NewYoutube(key string) (*YouTube, error) {
 // GetVideo returns the title and thumbnail of the given video
 func (y *YouTube) GetVideo(id string) *Video {
 	response, err := y.client.Videos.List([]string{"snippet", "contentDetails"}).Id(id).Do()
-	if err != nil || len(response.Items) == 0 {
+	if err != nil {
+		lit.Error("youtube GetVideo: %s", err.Error())
+		return nil
+	}
+
+	if len(response.Items) == 0 {
 		return nil
 	}
 
@@ -41,8 +47,13 @@ func (y *YouTube) GetVideo(id string) *Video {
 
 // GetPlaylist returns the URL, title and thumbnail of every element in the given playlist
 func (y *YouTube) GetPlaylist(id string) []Video {
-	response, err := y.client.PlaylistItems.List([]string{"snippet", "contentDetails"}).PlaylistId(id).Do()
-	if err != nil || len(response.Items) == 0 {
+	response, err := y.client.PlaylistItems.List([]string{"snippet"}).PlaylistId(id).MaxResults(50).Do()
+	if err != nil {
+		lit.Error("youtube GetPlaylist: %s", err.Error())
+		return nil
+	}
+
+	if len(response.Items) == 0 {
 		return nil
 	}
 
@@ -69,7 +80,12 @@ func (y *YouTube) GetPlaylist(id string) []Video {
 
 func (y *YouTube) getVideosDurations(id ...string) []float64 {
 	response, err := y.client.Videos.List([]string{"contentDetails"}).Id(id...).Do()
-	if err != nil || len(response.Items) == 0 {
+	if err != nil {
+		lit.Error("youtube getVideosDurations: %s", err.Error())
+		return nil
+	}
+
+	if len(response.Items) == 0 {
 		return nil
 	}
 
@@ -77,6 +93,35 @@ func (y *YouTube) getVideosDurations(id ...string) []float64 {
 	for _, item := range response.Items {
 		duration, _ := time.ParseDuration(strings.TrimPrefix(strings.ToLower(item.ContentDetails.Duration), "pt"))
 		result = append(result, duration.Seconds())
+	}
+
+	return result
+}
+
+// Search returns the URL, title and thumbnail of the first maxResults of the given query
+func (y *YouTube) Search(query string, maxResults int64) []Video {
+	response, err := y.client.Search.List([]string{"snippet"}).Q(query).Type("video").MaxResults(maxResults).Do()
+	if err != nil {
+		lit.Error("youtube Search: %s", err.Error())
+		return nil
+	}
+
+	result := make([]Video, 0, len(response.Items))
+	ids := make([]string, 0, len(response.Items))
+	for _, item := range response.Items {
+		result = append(result, Video{
+			URL:       "https://www.youtube.com/watch?v=" + item.Id.VideoId,
+			Title:     item.Snippet.Title,
+			Thumbnail: item.Snippet.Thumbnails.High.Url,
+			Duration:  0,
+		})
+
+		ids = append(ids, item.Id.VideoId)
+	}
+
+	durations := y.getVideosDurations(ids...)
+	for i := range result {
+		result[i].Duration = durations[i]
 	}
 
 	return result
--- youtube/youtube_test.go
+++ youtube/youtube_test.go
@@ -6,12 +6,18 @@ import (
 	"testing"
 )
 
-func TestYouTube_GetVideo(t *testing.T) {
-	yt, err := NewYoutube(os.Getenv("YOUTUBE_API"))
-	if err != nil {
-		t.Fatal(err)
-	}
+var yt *YouTube
 
+func init() {
+	var err error
+
+	yt, err = NewYoutube(os.Getenv("YOUTUBE_API"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestYouTube_GetVideo(t *testing.T) {
 	response := yt.GetVideo("dQw4w9WgXcQ")
 	if response == nil {
 		t.Fatal("video response is nil")
@@ -20,13 +26,26 @@ func TestYouTube_GetVideo(t *testing.T) {
 	if !strings.Contains(strings.ToLower(response.Title), "never gonna give you up") {
 		t.Fatal("video title does not match")
 	}
+}
 
+func TestYouTube_GetPlaylist(t *testing.T) {
 	result := yt.GetPlaylist("PLqcP8b8B-T6D9cru6eqsWQRLc8LlkIAKh")
 	if result == nil {
 		t.Fatal("playlist response is nil")
 	}
 
 	if result[0].URL != "https://www.youtube.com/watch?v=PvuYSybooLg" {
+		t.Fatal("first video does not match")
+	}
+}
+
+func TestYouTube_Search(t *testing.T) {
+	result := yt.Search("never gonna give you up", 1)
+	if result == nil {
+		t.Fatal("search response is nil")
+	}
+
+	if result[0].URL != "https://www.youtube.com/watch?v=dQw4w9WgXcQ" {
 		t.Fatal("first video does not match")
 	}
 }
