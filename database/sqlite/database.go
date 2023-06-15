02eb0384ce92687035090420b1664f1f5c49f514
Manuel Raimo <thetipo01@protonmail.com>
2023-06-15 17:46:27 +0200

    fix: blacklist table not saving new ids
    If the user had a recent account, the blacklist wouldn't work on them

--- database/sqlite/database.go
+++ database/sqlite/database.go
@@ -12,7 +12,7 @@ import (
 const (
 	tblSong      = "CREATE TABLE IF NOT EXISTS `song` (`id` varchar(200) NOT NULL, `title` varchar(200) NOT NULL, `duration` varchar(20) NOT NULL, `thumbnail` varchar(500) NOT NULL, `segments` varchar(1000) NOT NULL, PRIMARY KEY (`id`));"
 	tblCommands  = "CREATE TABLE IF NOT EXISTS `customCommands` (`guild` varchar(18) NOT NULL, `command` varchar(100) NOT NULL, `song` varchar(100) NOT NULL, `loop` tinyint(1) NOT NULL DEFAULT 0,  PRIMARY KEY (`guild`,`command`));"
-	tblBlacklist = "CREATE TABLE IF NOT EXISTS `blacklist`(`id` VARCHAR(18) NOT NULL, PRIMARY KEY (`id`));"
+	tblBlacklist = "CREATE TABLE IF NOT EXISTS `blacklist`(`id` VARCHAR(20) NOT NULL, PRIMARY KEY (`id`));"
 	tblLink      = "create table if not exists link ( songID varchar(200) not null references song, link varchar(500) not null constraint link_pk primary key );"
 )
 
